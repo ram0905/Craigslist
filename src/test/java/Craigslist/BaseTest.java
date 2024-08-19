c73395d3582fe4cfe854c093065a179fe3911fd8
R.O. <ramos16qa@gmail.com>
2024-08-19 00:23:49 -0700

    Added List webelements

--- src/test/java/Craigslist/BaseTest.java
+++ src/test/java/Craigslist/BaseTest.java
@@ -3,9 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
