82a730ca71e1cca84c573ed8835ba005fc4e3537
R.O. <ramos16qa@gmail.com>
2025-03-18 22:38:38 -0700

    Code clean

--- src/test/java/Locators/SearchPage.java
+++ src/test/java/Locators/SearchPage.java
@@ -3,7 +3,6 @@ import Base.BaseTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 import java.io.IOException;
 import java.util.List;
@@ -15,24 +14,22 @@ public class SearchPage extends BaseTest {
         loadPropFile();
     }
 
-    String title = "tampa bay cars & trucks for sale - craigslist";
-
-    @FindBy (xpath = "//input[@enterkeyhint='search']")
-    By searchField = (xpath("//input[@enterkeyhint='search']"));
-    By offerLists = (xpath("//span[contains(text(),'Prius')]"));
+    private static final String title = "tampa bay cars & trucks for sale - craigslist";
+    private static final By searchField = (xpath("//input[@enterkeyhint='search']"));
+    private static final By offerLists = (xpath("//span[contains(text(),'Prius')]"));
 
     public void search() {
         driver.findElement(searchField).sendKeys(prop.getProperty("item") + "\n");
     }
 
     public void assertTitle() {
-        Assert.assertEquals(title, driver.getTitle());
+        Assert.assertEquals(driver.getTitle(), title);
     }
 
     public void validOffers() {
         List<WebElement> carList = driver.findElements(offerLists);
-        for (int i = 0; i < carList.size(); i++) {
-            Assert.assertTrue(carList.get(i).getText().toLowerCase().contains(prop.getProperty("item")));
+        for (WebElement list : carList) {
+            Assert.assertTrue(list.getText().toLowerCase().contains(prop.getProperty("item")));
         }
         System.out.println("Total found  " + carList.size());
     }
